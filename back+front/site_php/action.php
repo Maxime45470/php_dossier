7096e1993b7f68681a7a8c38dff46a1ccab9909e
Jean-Maxime Dereuder <101884584+Maxime45470@users.noreply.github.com>
2022-06-15 21:51:35 +0200

    dossier back et front site ecommerce

---
+++ back+front/site_php/action.php
@@ -0,0 +1,265 @@
+<?php
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+ini_set('display_errors',false);
+require_once('functions.php');
+$pdo = pdo_connect();
+switch($_GET['e'])
+{
+    case 'inscription':
+
+        if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) && !empty($_POST['telephone']))
+        {
+            $_SESSION['formulaire'] = serialize($_POST);
+            if(is_uploaded_file($_FILES['photo']['tmp_name']))
+            {
+                // on upload la photo avec la fonction. 
+                $nom_image = uploadFichier($_FILES['photo']);
+                if(!$nom_image)
+                {
+                    // On redirige vers le formulaire d'inscription
+                    header('location:inscription.php?ERR=1');
+                    exit;
+                }
+                if(filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+                {
+                    $mot_de_passe = mot_de_passe();
+                    $methode1 = md5(sha1($mot_de_passe));
+                    //$methode2 = password_hash($mot_de_passe,PASSWORD_BCRYPT);
+                    $file = strip_tags($_POST['nom'].'-'.$_POST['prenom']).'.csv';
+                    $sql = 'INSERT INTO `admin` SET nom = "'.strip_tags($_POST['nom']).'", 
+                                                    prenom = "'.strip_tags($_POST['prenom']).'",
+                                                    email = "'.strip_tags($_POST['email']).'",
+                                                    telephone = "'.strip_tags($_POST['telephone']).'",
+                                                    photo = "'.$nom_image.'",
+                                                    mdp = "'.$methode1.'",
+                                                    Date_inscription = CURDATE()';
+                    $pdo->exec($sql);  
+                    // Code pour enregistrer le mot de passe en clair (uniquement en préprod !!!!)
+                    //if($_SERVER['HTTP_HOST'] == 'localhost')
+                    //mail($_POST['email'],'Accès Admin',$mot_de_passe);                              
+                    setcookie('passwd_clair',$mot_de_passe,(time()+3600));
+
+                    $fp = fopen($file,'x');
+
+                    $donnees = array(
+                        array('Nom','Prénom','Email','Mot de passe','Téléphone','URL de la photo'),
+                        array(strip_tags($_POST['nom']),strip_tags($_POST['prenom']),strip_tags($_POST['email']),$methode2,strip_tags($_POST['telephone']),$nom_image)
+                    ); 
+                    foreach($donnees as $ligne)
+                    {
+                        fputcsv($fp,$ligne);
+                    }  
+                    fclose($fp);
+                    $_SESSION['email'] = strip_tags($_POST['email']);
+                    setcookie('passwd',$methode1,(time()+86400));
+                    header('location:login.php');
+                    exit;
+
+                }
+                else
+                {
+                    header('location:inscription.php?ERR=2');
+                    exit;
+                }
+            }
+            else
+            {
+                header('location:inscription.php?ERR=3');
+                exit;
+            }
+        }
+        else
+        {
+            if(empty($_POST['nom']))
+                $err = 4;
+            else if(empty($_POST['prenom']))
+                $err = 5;
+            else if(empty($_POST['email']))
+                $err = 6;
+            else if(empty($_POST['telephone']))
+                $err = 7;
+            
+            header('location:inscription.php?ERR='.$err);
+            exit;
+        }
+
+    break;
+
+    case 'connection':
+        
+        if(!empty($_POST['email']) && !empty($_POST['password'])) 
+        {
+            $req = $pdo->prepare('SELECT * FROM `admin` WHERE email = "'.strip_tags($_POST['email']).'" AND mdp = "'.md5(sha1($_POST['password'])).'" ORDER BY ID DESC LIMIT 1');
+            echo 'SELECT * FROM `admin` WHERE email = "'.strip_tags($_POST['email']).'" AND mdp = "'.md5(sha1($_POST['password'])).'" ORDER BY ID DESC LIMIT 1';
+            $req->execute();
+            $resultat = $req->fetch(PDO::FETCH_OBJ);
+            if($resultat)
+            {
+                setcookie('id',$resultat->ID,(time()+31536000));
+                setcookie('password',$resultat->mdp,(time()+31536000));
+                $_SESSION['connect'] = $resultat->ID;
+                header('location:index.php');
+                exit;
+            }
+            else{
+                header('location:login.php?MSG=1');
+                exit;
+            }
+        }
+
+    break;
+
+
+}
+if(verifConnect())
+{ 
+    switch($_GET['uc'])
+    {
+        case 'ajout-actu':
+
+            if(!empty($_POST['titre']) && !empty($_POST['contenu']) && is_uploaded_file($_FILES['image']['tmp_name']))
+            {
+                $nom_fichier = uploadFichier($_FILES['image']);
+                if($nom_fichier)
+                {
+                    $req = 'INSERT INTO `actualites` SET
+                        titre = "'.strip_tags($_POST['titre']).'",
+                        contenu = "'.strip_tags($_POST['contenu']).'",
+                        image = "'.$nom_fichier.'",
+                        etat = "'.intval($_POST['etat']).'",
+                        date_ajout = CURDATE(),
+                        date_modification = CURDATE()';
+                    if($pdo->exec($req))
+                    {
+                        header('location:index.php?c=gestion-actus&MSG=1');
+                        exit;
+                    }
+                    else
+                    {
+                        $_SESSION['form-actus'] = serialize($_POST);
+                        header('location:index.php?c=form-actus&ERR=1');
+                        exit;
+                    }
+                }
+                else{
+                    $_SESSION['form-actus'] = serialize($_POST);
+                    header('location:index.php?c=form-actus&ERR=2');
+                    exit;
+                }
+            }
+            else
+            {
+                $_SESSION['form-actus'] = serialize($_POST);
+                if(empty($_POST['titre'])) $err = 3;
+                else if(empty($_POST['contenu'])) $err = 4;
+                header('location:index.php?c=form-actus&ERR=' .$err);
+                exit;
+            }
+
+        break;
+
+        case 'edit-actu':
+
+            if(!empty($_POST['id-actu']))
+            {
+                $actu = $pdo->prepare('SELECT * FROM `actualites` WHERE ID = "'.intval($_POST['id-actu']).'" AND etat != 3');
+                $actu->execute();
+                if($actu->rowCount() == 1)
+                {
+                    if(!empty($_POST['titre']) && !empty($_POST['contenu']))
+                    {
+                        $req = 'UPDATE `actualites` SET
+                                titre = "'.strip_tags($_POST['titre']).'",
+                                contenu = "'.strip_tags($post['contenu']).'",
+                                etat = "'.intval($_POST['etat']).'",
+                                dote_modification = CURDATE()
+                                WHERE ID = "'.intval($_POST['id-actu']).'"';
+                        if(!$pdo->exec($req))
+                        {
+                            header('location:index.php?c=form-actus&id='.intval($_POST['id-actu']).'&ERR=3');
+                            exit;
+                        }
+                        if(is_uploaded_file($_FILES['image']['tmp_name']))
+                        {
+                            $new_img = uploadFichier($_FILES['image']);
+                            if($new_img)
+                            {
+                                if($_POST['image_garde'] == 1)
+                                {
+                                    $actualite = $actu->fetchObject();
+                                    unlink($actualite->image);
+                                }
+                                $req2 = 'UPDATE `actualites` SET
+                                            image = "'.$new_img.'",
+                                            date_modification = CUREDATE()
+                                            WHERE ID = "'.intval($_POST['id-actu']).'"';
+                                if(!$pdo->exec($req2))
+                                {
+                                    header('location:index.php?c=form-actus&id='.intval($_POST['id-actu']).'&ERR=4');
+                                    exit;
+                                }
+                                else
+                                {
+                                    header('location:index.php?c=form-actus&id='.intval($_POST['id-actu']).'&ERR=5');
+                                    exit;
+                                }
+                            }
+                        }
+                        else 
+                        {
+                            //si l'un des champs est vide
+                            if(empty($_POST['titre'])) $err = 5;
+                            else if(empty($_POST['contenu'])) $err = 6; 
+                        }
+                        header('location:index.php?c=form-actus&id='.intval($_POST['id-actu']).'&ERR='.$err);
+                        exit;
+                    }
+                }
+                else
+                {
+                    header('location:index.php?c=gestion-actus&ERR=2');
+                    exit;
+                }
+            }
+            else
+            {
+                //si on a pas sélectionné d'actualité
+                header('location:index.php?c=gestion-actus&ERR=2');
+                exit;
+            }
+            
+
+        break;
+
+        case 'del-actu':
+
+            if(!empty($_GET['id']))
+            {
+                $req = 'UPDATE `actualites` SET
+                            etat = 3,
+                            date_modification = CURDATE()
+                            WHERE ID = "'.intval($_GET['id']).'"';
+                if($pdo->exec($req))
+                {
+                    header('location:index.php?c=gestion-actus&MSG=2');
+                    exit;
+                }
+                else
+                {
+                    header('location:index.php?c=gestion-actus&ERR=7');
+                    exit;
+                }
+
+            }
+            else
+            {
+                header('location:index.php?c=gestion-actus&ERR=8');
+                exit;
+            }
+
+        break;
+
+    }
+}
+?>
---
+++ back+front/site_php/index.php
@@ -0,0 +1,84 @@
+<?php
+// On ouvre  la session
+session_start();
+// On importe notre fichier functions.php qui contient la connection à la BDD
+require_once('functions.php');
+// On appelle notre function pdo_connect() pour effectuer la connexion à la BDD
+$pdo = pdo_connect();
+// On va vérifier l'existence des cookies ID et password 
+if(!verifConnect())
+{
+    header('location:login.php');
+    exit;
+}
+// Affichage de notre page
+// On affiche notre header
+require_once('header.php');
+// On fait un switch pour charger les différentes pages
+switch($_GET['c'])
+{
+    // Pour afficher la liste des actualités
+    case 'gestion-actus':
+        require_once('gestion-actus.php');
+    break;
+    // Formulaire d'ajout/edition des actualités
+    case 'form-actus':
+        require_once('form-actus.php');
+    break;
+    // Gestion des catégories
+    case 'gestion-categories':
+        require_once('gestion-categories.php');
+    break;
+    // Formulaire d'ajout/modification des catégories
+    case 'form-categories':
+        require_once('form-categories.php');
+    break;
+    // Gestion des pages
+    case 'gestion-pages':
+        require_once('gestion-pages.php');
+    break;
+    // Formulaire ajout/modification page
+    case 'form-pages':
+        require_once('form-pages.php');
+    break;
+    // Gestion des utilisateurs
+    case 'gestion-users':
+        require_once('gestion-users.php');
+    break;
+    // Formulaire ajout/modification des utilisateurs
+    case 'form-users':
+        require_once('form-users.php');
+    break;
+    // Gestion des paragraphes
+    case 'gestion-paras':
+        // On vérifie que l'ID de la page est bien passé en GET
+        if(!empty($_GET['pid']))
+            require_once('gestion-paras.php');
+        else
+        {
+            // Sinon on redirige l'utilisateur vers la liste des pages
+            header('location:index.php?c=gestion-pages');
+            exit;
+        }
+    break;
+    // Formulaire d'ajout/modification des paragraphes
+    case 'form-paras':
+        // On vérifie que l'ID de la page est bien passé en GET
+        if(!empty($_GET['pid']))
+            require_once('form-paras.php');
+        else
+        {
+            // Sinon on redirige l'utilisateur vers  la liste des pages
+            header('location:index.php?c=gestion-pages');
+            exit;
+        }
+    break;
+    // Page par défaut du backoffice
+    default:
+        header('location:404.php');
+    break;
+}
+
+// On affiche notre footer
+require_once('footer.php');
+?>
